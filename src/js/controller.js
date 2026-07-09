0dfb06eadb46bbf7061ddd4a925fe2540b640920
MrBimali235 <subhambimali5@gmail.com>
2026-07-09 23:41:50 +0545

    Exported state and loadRecipe from model.js file , imported it in controller.js which uses the updated state to render the UI for user action

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,4 @@
+import * as model from "./model.js";
 import icons from "url:../img/icons.svg";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
@@ -28,28 +29,8 @@ const showRecipe = async function () {
     const id = window.location.hash.slice(1);
     if (!id) return;
     renderSpinner(recipeContainer);
-    const res = await fetch(
-      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
-    );
-    const data = await res.json();
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-    console.log(data);
-    let { recipe } = data.data;
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
-
-    console.log(recipe);
-
-    //Rendering recipe
-
+    await model.loadRecipe(id);
+    const { recipe } = model.state;
     const markup = `  <figure class="recipe__fig">
           <img src="${recipe.image}" alt="${
       recipe.title
