44813e545fa915fe0a21718f271c1abf2e597ec5
MrBimali235 <subhambimali5@gmail.com>
2026-07-12 22:16:36 +0545

    The recipe control function is refactored

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,5 @@
 import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
-import icons from "url:../img/icons.svg";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 const recipeContainer = document.querySelector(".recipe");
@@ -13,27 +12,18 @@ const timeout = function (s) {
   });
 };
 
-const renderSpinner = function (parentEl) {
-  const markup = `
-  <div class="spinner">
-          <svg>
-            <use href="${icons}.svg#icon-loader"></use>
-          </svg>
-        </div>`;
-  parentEl.innerHTML = "";
-  parentEl.insertAdjacentHTML("afterbegin", markup);
-};
-
-const showRecipe = async function () {
+const controlRecipes = async function () {
   try {
     //Loading the recipe
     const id = window.location.hash.slice(1);
     if (!id) return;
-    renderSpinner(recipeContainer);
+    recipeView.renderSpinner();
     await model.loadRecipe(id);
     recipeView.render(model.state.recipe);
   } catch (err) {
     alert(err);
   }
 };
-["hashchange", "load"].forEach((ev) => window.addEventListener(ev, showRecipe));
+["hashchange", "load"].forEach((ev) =>
+  window.addEventListener(ev, controlRecipes)
+);
