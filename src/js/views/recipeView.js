320882cd9cc2f49b85be0fdb95b741aa7e4d4cbb
MrBimali235 <subhambimali5@gmail.com>
2026-07-12 22:14:12 +0545

    Defined the render function which generates the markup , clears the parent element container and inserts the generated markup , render spinner function is also defined here

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,16 +1,35 @@
+import icons from "url:../../img/icons.svg";
 class RecipeView {
   #parentElement = document.querySelector(".recipe");
   #data;
   render(data) {
     this.#data = data;
+    const markup = this.#generateMarkUp();
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML("afterbegin", markup);
   }
+  #clear() {
+    this.#parentElement.innerHTML = "";
+  }
+
+  renderSpinner = function () {
+    const markup = `
+  <div class="spinner">
+          <svg>
+            <use href="${icons}.svg#icon-loader"></use>
+          </svg>
+        </div>`;
+    this.#parentElement.innerHTML = "";
+    this.#parentElement.insertAdjacentHTML("afterbegin", markup);
+  };
+
   #generateMarkUp() {
     return `  <figure class="recipe__fig">
-              <img src="${recipe.image}" alt="${
-      recipe.title
+              <img src="${this.#data.image}" alt="${
+      this.#data.title
     }" class="recipe__img" />
               <h1 class="recipe__title">
-                <span>${recipe.title}</span>
+                <span>${this.#data.title}</span>
               </h1>
             </figure>
     
@@ -20,7 +39,7 @@ class RecipeView {
                   <use href="${icons}#icon-clock"></use>
                 </svg>
                 <span class="recipe__info-data recipe__info-data--minutes">${
-                  recipe.cookingTime
+                  this.#data.cookingTime
                 }</span>
                 <span class="recipe__info-text">minutes</span>
               </div>
@@ -29,7 +48,7 @@ class RecipeView {
                   <use href="${icons}#icon-users"></use>
                 </svg>
                 <span class="recipe__info-data recipe__info-data--people">${
-                  recipe.servings
+                  this.#data.servings
                 }</span>
                 <span class="recipe__info-text">servings</span>
     
@@ -62,7 +81,7 @@ class RecipeView {
             <div class="recipe__ingredients">
               <h2 class="heading--2">Recipe ingredients</h2>
               <ul class="recipe__ingredient-list">
-              ${recipe.ingredients
+              ${this.#data.ingredients
                 .map((ing) => {
                   return `<li class="recipe__ingredient">
                   <svg class="recipe__icon">
@@ -84,13 +103,13 @@ class RecipeView {
               <p class="recipe__directions-text">
                 This recipe was carefully designed and tested by
                 <span class="recipe__publisher">${
-                  recipe.publisher
+                  this.#data.publisher
                 }</span>. Please check out
                 directions at their website.
               </p>
               <a
                 class="btn--small recipe__btn"
-                href="${recipe.sourceUrl}"
+                href="${this.#data.sourceUrl}"
                 target="_blank"
               >
                 <span>Directions</span>
@@ -99,8 +118,6 @@ class RecipeView {
                 </svg>
               </a>
             </div>`;
-    recipeContainer.innerHTML = "";
-    recipeContainer.insertAdjacentHTML("afterbegin", markup);
   }
 }
 export default new RecipeView();
