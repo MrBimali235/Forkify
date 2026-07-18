e5ac918669cd6e2387ccb76b3b51c50ec3b3d09f
MrBimali235 <subhambimali5@gmail.com>
2026-07-18 23:40:56 +0545

    Removed logging functions

--- src/js/model.js
+++ src/js/model.js
@@ -32,7 +32,6 @@ export const loadSearchResults = async function (query) {
   try {
     state.search.query = query;
     const data = await getJSON(`${API_URL}?search=${query}`);
-    console.log(data);
     state.search.results = data.data.recipes.map((rec) => {
       return {
         id: rec.id,
@@ -42,10 +41,8 @@ export const loadSearchResults = async function (query) {
         image: rec.image_url,
       };
     });
-    console.log(state.search.results);
   } catch (err) {
     console.error(`${err}`);
     throw err;
   }
 };
-loadSearchResults("pizza");
