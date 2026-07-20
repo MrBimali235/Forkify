d2a4fed594fc301f32cb140da6020e98932c20cd
MrBimali235 <subhambimali5@gmail.com>
2026-07-20 22:19:03 +0545

    The paginationView file is defined which contains the PaginationView class and different conditions for different markups are commented

---
+++ src/js/views/paginationView.js
@@ -0,0 +1,11 @@
+import icons from "url:../../img/icons.svg";
+import View from "./View";
+class PaginationView extends View {
+  _parentElement = document.querySelector(".pagination");
+  _generateMarkup() {
+    //page 1 , and there are other pages
+    //page 1 , and there are no other pages
+    //last page
+    //Other page
+  }
+}
