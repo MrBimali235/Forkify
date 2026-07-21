88747c4fe62a68188f2041948ad6818c8e8ca826
MrBimali235 <subhambimali5@gmail.com>
2026-07-21 23:18:51 +0545

    Calculated the no of pages

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -3,9 +3,11 @@ import View from "./View";
 class PaginationView extends View {
   _parentElement = document.querySelector(".pagination");
   _generateMarkup() {
+    const numPages = this._data.results / this._data.resultsPerPage;
     //page 1 , and there are other pages
     //page 1 , and there are no other pages
     //last page
     //Other page
   }
 }
+export default new PaginationView();
