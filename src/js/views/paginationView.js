c3fc6b2e4d19efb79db31f8aac9cfccb54ad6a64
MrBimali235 <subhambimali5@gmail.com>
2026-07-22 23:15:54 +0545

    Defined markUp for different cases of different no of search results

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -3,11 +3,48 @@ import View from "./View";
 class PaginationView extends View {
   _parentElement = document.querySelector(".pagination");
   _generateMarkup() {
-    const numPages = this._data.results / this._data.resultsPerPage;
+    const curPage = this._data.page;
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+    console.log(numPages);
     //page 1 , and there are other pages
-    //page 1 , and there are no other pages
+    if (curPage === 1 && numPages > 1) {
+      return `<button class="btn--inline pagination__btn--next">
+            <span>Page ${curPage + 1}</span>
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-right"></use>
+            </svg>
+          </button>`;
+    }
+
     //last page
+    if (curPage === numPages && numPages > 1) {
+      return `<button class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-left"></use>
+            </svg>
+            <span>Page ${curPage - 1}</span>
+          </button>`;
+    }
     //Other page
+    if (curPage < numPages) {
+      return `<button class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-left"></use>
+            </svg>
+            <span>Page ${curPage - 1}</span>
+          </button>
+          <button class="btn--inline pagination__btn--next">
+            <span>Page ${curPage + 1}</span>
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-right"></use>
+            </svg>
+          </button> 
+          `;
+    }
+    //page 1 , and there are no other pages
+    return "";
   }
 }
 export default new PaginationView();
