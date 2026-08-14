3c2295f4cf4fea9431cdb0632f357baa51ef1d11
MrBimali235 <subhambimali5@gmail.com>
2026-08-15 00:26:20 +0545

    Created a controlAddBookmark function and passed it to the view following publisher-subscriber pattern

--- src/js/controller.js
+++ src/js/controller.js
@@ -52,9 +52,15 @@ const controlServings = function (newServings) {
   recipeView.update(model.state.recipe);
 };
 
+const controlAddBookmark = function () {
+  model.addBoookmark(model.state.recipe);
+  recipeView.update(model.state.recipe);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
