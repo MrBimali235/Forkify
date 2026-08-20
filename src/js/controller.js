a33196ab829f8fec4153c1135f2824a6beb99797
MrBimali235 <subhambimali5@gmail.com>
2026-08-20 23:54:07 +0545

    set up the reference between addRecipeView and controller using the publisher subscriber pattern

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,6 +6,7 @@ import searchView from "./views/searchView.js";
 import resultsView from "./views/resultsView.js";
 import paginationView from "./views/paginationView.js";
 import bookmarksView from "./views/bookmarksView.js";
+import addRecipeView from "./views/addRecipeView.js";
 import { addBoookmark } from "./model.js";
 import { deleteBookmark } from "./model.js";
 
@@ -63,6 +64,8 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlAddRecipe = function (newRecipe) {};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -70,5 +73,6 @@ const init = function () {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 init();
