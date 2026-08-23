3b6b181964d498bdb6e038c8343439c19b56d50b
MrBimali235 <subhambimali5@gmail.com>
2026-08-23 23:29:32 +0545

    Defined the message to be rendered

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -2,6 +2,7 @@ import icons from "url:../../img/icons.svg";
 import View from "./View";
 class AddRecipeView extends View {
   _parentElement = document.querySelector(".upload");
+  _message = "Recipe was successfully uploaded";
   _window = document.querySelector(".add-recipe-window");
   _overlay = document.querySelector(".overlay");
   _btnOpen = document.querySelector(".nav__btn--add-recipe");
